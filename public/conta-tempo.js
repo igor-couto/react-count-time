79fdf59438415e7693f2c1d3224272ce7fea46d2
Igor Couto <igor.fcouto@gmail.com>
2018-02-28 23:44:38 -0300

    Fix minor identation problem

--- public/conta-tempo.js
+++ public/conta-tempo.js
@@ -1,16 +1,17 @@
-  class ExampleApplication extends React.Component {
-    render() {
-      var elapsed = Math.round(this.props.elapsed  / 100);
-      var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
-      var message =
-        `O React está rodando a ${seconds} segundos.`;
-        return <p>{message}</p>;
-    }
+class ExampleApplication extends React.Component {
+  render() {
+    var elapsed = Math.round(this.props.elapsed  / 100);
+    var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
+    var message =
+      `O React está rodando a ${seconds} segundos.`;
+      return <p>{message}</p>;
   }
-  var start = new Date().getTime();
-    setInterval(() => {
-      ReactDOM.render(
-      <ExampleApplication elapsed={new Date().getTime() - start} />,
-      document.getElementById('container')
-    );
-  }, 50);
+}
+
+var start = new Date().getTime();
+setInterval(() => {
+  ReactDOM.render(
+    <ExampleApplication elapsed={new Date().getTime() - start} />,
+    document.getElementById('container')
+  );
+}, 50);
